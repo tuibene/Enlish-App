02532002311305795e08ff11bb2981d95b17a0eb
tuibene <thaibao1372002@gmail.com>
2026-04-28 18:42:13 +0700

    fix: add proxy:true to passport to resolve redirect_uri_mismatch on Render

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -6,6 +6,7 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: '/api/users/auth/google/callback',
+    proxy: true
 },
     async (accessToken, refreshToken, profile, done) => {
         try {
